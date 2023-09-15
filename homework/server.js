ae7ceb128d40a3ed49e72b51a2f5e20df7eba196
foe_la <f.catrina615@gmail.com>
2023-09-15 15:51:55 -0400

    8Ball

--- homework/server.js
+++ homework/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
+const aRandomResponse = require(["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+);
+
 app.get("/greeting", function (req, res) {
     res.send("<h1>Hello, stranger</h1>");
   });
 
   app.get("/tip", function (req, res) {
     res.send("<h2>Tip Calculator</h2>");
+  });
+
+  app.get("/magic", function (req, res) {
+    res.send("<h1> Magic 8 Ball </h1>");
+  });
+
+  app.get("/magic/:randomResponse", function (req, res) {
+    console.log(randomResponse);
+    let aRandomResponse = randomResponse[Math.floor(Math.random()*randomResponse.length)];
+    res.send("<h1> ${req.params.randomResponse} , ${aRandomResponse} </h1>
+    );
   });
 
   app.get("/tip/:total", function (req, res) {
