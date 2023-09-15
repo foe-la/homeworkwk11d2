964d129c51d0294f00cac90d8bf1ff6c18eec274
foe_la <f.catrina615@gmail.com>
2023-09-15 15:36:10 -0400

    tipCalc

--- homework/server.js
+++ homework/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get("/greeting/", function (req, res) {
+app.get("/greeting", function (req, res) {
     res.send("<h1>Hello, stranger</h1>");
   });
+
+  app.get("/tip", function (req, res) {
+    res.send("<h2>Tip Calculator</h2>");
+  });
+
+  app.get("/tip/:total", function (req, res) {
+    res.send("Total: " + req.params.total);
+  });
+
+  app.get("/tip/:total/:tipPercentage", function (req, res) {
+    res.send("Tip: " + ((req.params.tipPercentage / 100) * req.params.total));
+  });
+
   app.get("/greeting/:name", function (req, res) {
   
     res.send("Wow! Hello there " + req.params.name);
